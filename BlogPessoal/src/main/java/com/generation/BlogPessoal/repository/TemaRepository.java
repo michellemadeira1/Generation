1badc7e97dcbab512d3c3c41a9048ed9007f3220
Michelle Madeira <michellemadeira1@gmail.com>
2022-02-21 19:56:44 -0300

    BloguePessoal

---
+++ BlogPessoal/src/main/java/com/generation/BlogPessoal/repository/TemaRepository.java
@@ -0,0 +1,12 @@
+package com.generation.BlogPessoal.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.BlogPessoal.model.Tema;
+
+public interface TemaRepository extends JpaRepository<Tema, Long>{
+	
+          public List<Tema> findAllByDescricaoContaingIgnoreCasa(String descricao);
+}
